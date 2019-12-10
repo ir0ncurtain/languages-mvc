eff6a0a049b1340b6efe28c2da42714ec92a178b
ir0ncurtain <michal4vision@gmail.com>
2019-12-10 09:23:07 +0100

    add resources

---
+++ languages-mvc/Models/Register.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace languages_mvc.Models
+{
+    public class Register
+    {
+        [Display(Name = "FirstName", ResourceType = typeof(Resources.Resource))]
+        [Required(ErrorMessageResourceType = typeof(Resources.Resource), ErrorMessageResourceName = "FirstNameRequired")]
+        public string FirstName
+        {
+            get;
+            set;
+        }
+        [Display(Name = "LastName", ResourceType = typeof(Resources.Resource))]
+        [Required(ErrorMessageResourceType = typeof(Resources.Resource), ErrorMessageResourceName = "LastNameRequired")]
+        public string LastName
+        {
+            get;
+            set;
+        }
+        [Display(Name = "Email", ResourceType = typeof(Resources.Resource))]
+        [Required(ErrorMessageResourceType = typeof(Resources.Resource), ErrorMessageResourceName = "EmailRequired")]
+        [RegularExpression(@"^([0-9a-zA-Z]([\+\-_\.][0-9a-zA-Z]+)*)+@(([0-9a-zA-Z][-\w]*[0-9a-zA-Z]*\.)+[a-zA-Z0-9]{2,3})$", ErrorMessageResourceType = typeof(Resources.Resource), ErrorMessageResourceName = "EmailInvalid")]
+        public string Email
+        {
+            get;
+            set;
+        }
+    }
+}
